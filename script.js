11431092c3069003f6f31e8595279b0ce62f587a
aru99 <mohammadarmanmavric@gmail.com>
2021-12-02 13:58:14 +0530

    added modal functionality, open and close

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log(document.querySelector('body').textContent);
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log(btnsOpenModal[i].textContent);
+    //while selecting the class here, we do not use the dot '.'
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
